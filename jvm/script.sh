fcb17610c43ca161bd7479cda8439af6c6397094
Taewoo An (Alan) <alan@chai.finance>
2023-03-03 14:49:54 +0900

    [JVM] script.sh, jvm action 오타 수정 및 action cache 해제 (#3)

--- jvm/script.sh
+++ jvm/script.sh
@@ -3,8 +3,8 @@ export GRPC_HEALTH_PROBE_VERSION=v0.4.14
 
 ARCH=""
 case $(arch) in
-    x86_64) ARCH="amd64" ;;
-    aarch64) ARCH="arm64" ;;
+    x86_64) ARCH=amd64 ;;
+    aarch64) ARCH=arm64 ;;
 esac
 
 echo $ARCH
@@ -15,5 +15,6 @@ apt-get install -y wget
 
 wget -O /dd-java-agent.jar https://dtdg.co/latest-java-tracer
 
-wget -qO/bin/grpc_health_probe https://github.com/grpc-ecosystem/grpc-health-probe/releases/download/${GRPC_HEALTH_PROBE_VERSION}/grpc_health_probe-linux-{$ARCH}
+wget -O /bin/grpc_health_probe https://github.com/grpc-ecosystem/grpc-health-probe/releases/download/${GRPC_HEALTH_PROBE_VERSION}/grpc_health_probe-linux-${ARCH}
 chmod +x /bin/grpc_health_probe
+
